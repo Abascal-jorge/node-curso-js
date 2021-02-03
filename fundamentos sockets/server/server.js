3c08b18ece1bd7b2563193ca8fe1c75767ed94c1
jorge abascal lopez <jorgeabascal20@gmail.com>
2021-02-02 23:36:21 -0500

    Socket chat cors

--- fundamentos sockets/server/server.js
+++ fundamentos sockets/server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const path = require("path");
+//const path = require("path");
 const socketIO = require("socket.io");
 const cors = require("cors");
 const http = require("http");
 const app = express();
-const server = http.createServer(app);
+
 
 //Habilitar cors
 const opcionesCors = {
@@ -13,20 +13,35 @@ const opcionesCors = {
 app.use( cors(opcionesCors) );
 /////////////////
 
-
-const publicPath = path.resolve(__dirname, `../public`);
+//const publicPath = path.resolve(__dirname, `../public`);
 const port = process.env.PORT || 4000;
 
-app.use(express.static(publicPath));
+app.get("/usuario", (req, res) => {
+    res.json({
+        message: "Desde usuario get"
+    });
+});
+
+
+//app.use(express.static(publicPath));
+
+app.listen(4000, (error) => {
+    if(error){
+        console.log(error);
+    }
+    console.log("Conectado al servidor....");
+});
+
+
+/*Codigo a quitar
+
+const server = http.createServer(app);
+
+
 let io = socketIO(server);
 
 io.on("connection", (client) => {
     console.log("hola");
 });
 
-server.listen(port, (error) => {
-    if(error){
-        console.log(error);
-    }
-    console.log("Conectado al servidor....");
-});
+*/
