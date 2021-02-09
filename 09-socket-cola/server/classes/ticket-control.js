73013029edfaa3a2144f5b41011e2b24e47406fb
jorge abascal lopez <jorgeabascal20@gmail.com>
2021-02-08 22:51:12 -0500

    Agregando definir ticket a despechar

--- 09-socket-cola/server/classes/ticket-control.js
+++ 09-socket-cola/server/classes/ticket-control.js
@@ -13,11 +13,13 @@ class TicketCreado{
         this.ultimo = 0;
         this.hoy = new Date().getDate();
         let tickets = [];
+        let ultimos4 = [];
 
         let data = require("../data/data.json");
         if( this.hoy === data.hoy){
             this.ultimo = data.ultimo;
             this.tickets = data.tickets;
+            this.ultimos4 = data.ultimos4;
         }else{
             this.reiniciarTicket();
         }
@@ -42,18 +44,40 @@ class TicketCreado{
         }
 
         let numeroTicket = this.tickets[0].numero;
+        this.tickets.shift();
+
+        let atenderTicket = new Ticket(numeroTicket, escritorio);
+
+        this.ultimos4.unshift( atenderTicket );
+
+        if( this.ultimos4.length > 4 ){
+            this.ultimos4.splice(-1, 1);
+        }
+
+        console.log( this.ultimos4 );
+
+        this.grabarArchivo();
+
+        return  atenderTicket;
     }
 
     reiniciarTicket(){
         this.ultimo = 0;
         this.tickets = [];
+        this.ultimos4 = [];
         this.grabarArchivo();
     }
 
     grabarArchivo(){
-        let info =  JSON.stringify({ ultimo: this.ultimo, hoy: this.hoy, tickets: this.tickets});
+        let info =  JSON.stringify({ 
+            ultimo: this.ultimo, 
+            hoy: this.hoy, 
+            tickets: this.tickets,
+            ultimos4: this.ultimos4
+        });
         fs.writeFileSync("./server/data/data.json", info);
     }
+
 }
 
 module.exports = {
