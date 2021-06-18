4775984dee65d3b43f721f065f7903285c9134d8
unknown <jorgeabascal20@gmail.com>
2021-06-17 22:43:06 -0500

    solucionando error server

--- Api node con mysql/server/server.js
+++ Api node con mysql/server/server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
 //const dbConnection = require("../config/config");
 
 //dbConnection();
 app.use(express.urlencoded({ extended: false }));
-=======
-
->>>>>>> c10aa47091b338406d9bbf3ad5747d9070aa7202
 app.use( require("../route/index") );
 
 app.listen( 4000, "0.0.0.0", () => {
